e28a3e4832502a23ba04df65a0ce94fb66ddd4ee
Michael Vlamis <michael.vlamis@icloud.com>
2025-02-18 16:24:07 -0500

    reflection 1

---
+++ eventsWeek4/keyevent.js
@@ -0,0 +1,44 @@
+window.onload = function () {
+    console.log("keys");
+    let speedX = 5;
+
+    window.addEventListener("keydown", function (event) {
+        if (event.key === "ArrowRight") {
+
+            document.getElementById("boxA").style.left = parseInt(document.getElementById("boxA").style.left) + speedX + "px";
+        }
+        else if (event.key === "ArrowLeft") {
+
+            document.getElementById("boxA").style.left = parseInt(document.getElementById("boxA").style.left) - speedX + "px";
+        }
+        else if (event.code === "Space") {
+            let bool = document.getElementById("boxB").getAttribute("custom-bool");
+            if (bool === "off") {
+                document.getElementById("boxB").style.background = "orange";
+                document.getElementById("boxB").setAttribute("custom-bool", "on");
+            } else {
+                document.getElementById("boxB").style.background = "rgb(112, 184, 226)";
+                document.getElementById("boxB").setAttribute("custom-bool", "off");
+            }
+        }
+        else if (event.key === "Shift") {
+            document.getElementById("boxA").style.background = "rgb(226, 112, 177)";
+        }
+    })
+
+    window.addEventListener("keyup", function (event) {
+        console.log("keyup")
+        //2: change color when space is down
+        if (event.key === "Shift") {
+            document.getElementById("boxA").style.background = "rgb(112, 184, 226)";
+
+        }
+        else {
+            console.log(`key up not shift:`)
+            console.log(event)
+        }
+
+    })
+    
+
+}
